f6af85540238a0693d9e5ab23a16b4b29e7483bc
Deborah Pickett <github@dp.futzle.com>
2011-06-15 21:29:00 +0000

    Better graphs for two-hourly and daily history.

--- J_CurrentCostEnviR1.js
+++ J_CurrentCostEnviR1.js
@@ -52,36 +52,125 @@ function valueToChartSymbol(x)
 	}
 }
 
-/* Produce a Google Image Chart API image URI. */
-function graphHistory(h)
-{
-	var uri = "http://chart.apis.google.com/chart?chxr=0,-11.667,160&chxs=0,676767,10.5,0,l,676767&chxt=y&chbh=a,0&chs=400x150&cht=bvs&chco=4D89F9&chds=0,25&chd=t:"
-		+ h.map(valueToChartSymbol).reverse().join(",");
-		+ "&chg=-1,0&chma=|7&chtt=History";
-	return uri;
-}
-
 /* Two-hourly history for the last 7 days. */
-function showTwohourlyHistory(deviceId)
+function showTwohourlyHistory(deviceId, l)
 {
 	_console("start showTwohourlyHistory");
 	var twohourlyHistoryString;
 	twohourlyHistoryString = get_device_state(deviceId, "urn:futzle-com:serviceId:CurrentCostEnviR1", "TwoHourlyHistory", 1);
 	var historyObject = deserializeHistory(twohourlyHistoryString);
-	var historyArray = getHistoryArray(historyObject, 2, 24*7+2);
-	return "<img src=\"" + graphHistory(historyArray) + "\"/>";
+	var historyArray = getHistoryArray(historyObject, 2, 2*l);
+
+	_console("showTwohourlyHistory A");
+	var max = Math.ceil(historyArray.reduce(function (a, b) { if (a == undefined) return b; else { if (b == undefined) return a; else return Math.max(a, b); }}));
+
+	var uri = "http://chart.apis.google.com/chart";
+	uri += "?chxr=0,-11.667,160";
+
+	_console("showTwohourlyHistory C");
+	// Axes and legend.
+	var dates = new Array();
+	var i;
+	var j;
+	var today = new Date();
+	today.setMilliseconds(0);
+	today.setSeconds(0);
+	today.setMinutes(0);
+	today.setHours(today.getHours() + today.getHours() % 2 + 2);
+	_console("showTwohourlyHistory E");
+	var dayNames = ["S", "M", "T", "W", "T", "F", "S"];
+	var dateLegendText = "";
+	var datePosition = "";
+	_console("showTwohourlyHistory F");
+	var hoursIntoToday = today.getHours();
+	for (i = hoursIntoToday/2, j = 0; i < l + 6; i += 12, j++)
+	{
+	_console("showTwohourlyHistory G");
+		var time = new Date(today.getFullYear(), today.getMonth(), today.getDate() - j, 12, 0, 0, 0)
+	_console("showTwohourlyHistory H");
+
+		dateLegendText += "|" + time.getDate() + "|%C2%A0";
+	_console("showTwohourlyHistory I");
+		datePosition += "," + (i-6) + "," + i;
+	}
+	_console("showTwohourlyHistory M");
+	uri += "&chxl=1:" + dateLegendText;
+	uri += "&chxp=1" + datePosition;
+	uri += "&chxr=0,0," + max + "|1," + (l - 0.5) + ",0.5";
+	uri += "&chxs=0,676767,10.5,1,l,676767|1,676767,11.5,0,lt,676767";
+	uri += "&chxtc=1,0,5";
+	uri += "&chxt=y,x";
+
+	uri += "&chbh=a,1,0";
+	uri += "&chs=620x130";
+	uri += "&cht=bvs";
+	uri += "&chco=4D89F9";
+	// Y scale.
+	uri += "&chds=0," + max;
+	// Data.
+	uri += "&chd=t:"
+		+ historyArray.map(valueToChartSymbol).reverse().join(",");
+	uri += "&chg=-1,5,0,4";
+	// Chart title.
+	uri += "&chtt=Two-hourly+History";
+	_console("showTwohourlyHistory Z");
+	return "<img src=\"" + uri + "\"/>";
 }
 
-/* Daily history for the last 31 days. */
-function showDailyHistory(deviceId)
+/* Daily history for the last l days. */
+function showDailyHistory(deviceId, l)
 {
 	_console("start showDailyHistory");
 	var dailyHistoryString;
 	dailyHistoryString = get_device_state(deviceId, "urn:futzle-com:serviceId:CurrentCostEnviR1", "DailyHistory", 1);
 	var historyObject = deserializeHistory(dailyHistoryString);
-	var historyArray = getHistoryArray(historyObject, 1, 31);
-	return "<img src=\"" + graphHistory(historyArray) + "\"/>";
-	return historyArray;
+	var historyArray = getHistoryArray(historyObject, 1, l);
+
+	var max = Math.ceil(historyArray.reduce(function (a, b) { if (a == undefined) return b; else { if (b == undefined) return a; else return Math.max(a, b); }}));
+
+	var uri = "http://chart.apis.google.com/chart";
+	uri += "?chxr=0,-11.667,160";
+
+	// Axes and legend.
+	var dates = new Array();
+	var i;
+	// Around noon to avoid issues with daylight saving.
+	var today = new Date();
+	today.setHours(12);
+	var dayNames = ["S", "M", "T", "W", "T", "F", "S"];
+	var dayOfWeekLegendText = "";
+	var dateLegendText = "";
+	var dayOfWeekPosition = "";
+	var datePosition = "";
+	for (i = 1; i < l; i++)
+	{
+		var dayOfWeek = new Date(today.getFullYear(), today.getMonth(),
+			today.getDate() - i, 12, 0, 0, 0);
+		dayOfWeekLegendText += "|" + dayNames[dayOfWeek.getDay()];
+		dayOfWeekPosition += "," + i;
+		if (i % 2 == 0) { continue; }
+		dateLegendText += "|" + dayOfWeek.getDate();
+		datePosition += "," + i;
+	}
+	uri += "&chxl=1:" + dayOfWeekLegendText + "|2:" + dateLegendText;
+	uri += "&chxp=1" + dayOfWeekPosition + "|2" + datePosition;
+	uri += "&chxr=0,0," + max + "|1," + (l - 0.5) + ",0.5|2," + (l - 0.5) + ",0.5";
+	uri += "&chxs=0,676767,10.5,1,l,676767";
+	uri += "&chxt=y,x,x";
+
+	uri += "&chbh=a,1,0";
+	uri += "&chs=620x130";
+	uri += "&cht=bvs";
+	uri += "&chco=4D89F9";
+	// Y scale.
+	uri += "&chds=0," + max;
+	// Data.
+	uri += "&chd=t:"
+		+ historyArray.map(valueToChartSymbol).reverse().join(",");
+	uri += "&chg=-1,5,0,4";
+	// Chart title.
+	uri += "&chtt=Daily+History";
+	return "<img src=\"" + uri + "\"/>";
 }
 
 /* Monthly history, to do */
@@ -92,11 +181,11 @@ function showHistory(deviceId)
 	var htmlResult = "";
 	
 	htmlResult += "<div>";
-	htmlResult += showTwohourlyHistory(deviceId);
+	htmlResult += showTwohourlyHistory(deviceId, 84);
 	htmlResult +="</div>";	
 
 	htmlResult += "<div>";
-	htmlResult += showDailyHistory(deviceId);
+	htmlResult += showDailyHistory(deviceId, 31);
 	htmlResult +="</div>";	
 
 	set_panel_html(htmlResult);
